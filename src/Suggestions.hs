3dd0c0b5304a9f5caa956289efad60d0bfd75889
Jimmy Ren <jimbuscus23@gmail.com>
2022-12-13 12:34:27 -0500

    added state transformer

--- src/Suggestions.hs
+++ src/Suggestions.hs
@@ -1,52 +1,56 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
 module Suggestions where
 
 import Checker qualified as C
+import Control.Monad.Except
+  ( ExceptT,
+    MonadError (throwError),
+    runExceptT,
+  )
+import Control.Monad.Identity
+  ( Identity (runIdentity),
+  )
+import Control.Monad.State
+  ( MonadState (get, put),
+    StateT (runStateT),
+  )
+import Data.Function ((&))
 import Data.Map (Map)
 import Data.Map qualified as Map
 import GHC.Base (undefined)
 import Parsing
 import ShellSyntax
-import State qualified as S
 import System.IO qualified as IO
 import System.IO.Error qualified as IO
 import TempParsing qualified as T
 
 -- | Action that updates the state
-updHistory :: BashCommand -> S.State (Map Var Expression) ()
+updHistory :: MonadState (Map Var Expression) m => BashCommand -> m ()
 updHistory bc = case bc of
   PossibleAssign var ex -> do
-    oldHistory <- S.get
+    oldHistory <- Control.Monad.State.get
     let newHistory = Map.insert var ex oldHistory
-    S.put newHistory
+    put newHistory
   Assign var ex -> do
-    oldHistory <- S.get
+    oldHistory <- Control.Monad.State.get
     let newHistory = Map.insert var ex oldHistory
-    S.put newHistory
+    put newHistory
   _ -> do
     return ()
 
-test :: ((), Map Var Expression)
-test = S.runState (updHistory (Assign (V "x") (Val (IntVal 3)))) Map.empty
-
--- >>> test
--- ((),fromList [(V "x",Val (IntVal 3))])
-
 -- | Read each line and update the history
-readFile :: FilePath -> Parser BashCommand -> IO ()
-readFile fileName parser = do
-  contents <- IO.readFile fileName
-  let lines = Prelude.lines contents
-  return ()
+-- readFile :: FilePath -> Parser BashCommand -> IO ()
+-- readFile fileName parser = do
+--   contents <- IO.readFile fileName
+--   let lines = Prelude.lines contents
+--   return ()
 
--- | Reads line and outputs the warnings
+-- | Reads line and outputs the warnings and updates the state
 parseLine :: String -> IO (Either ParseResult BashCommand)
 parseLine line = do
   pure $ parse T.bashCommandP line
 
--- | For each line, parse and update the history
-parseLines :: [String] -> S.State (Map Var Expression) ()
+-- | For each line, parse and update the history and output any errors
+parseLines :: [String] -> StateT (Map Var Expression) IO ()
 parseLines (x : xs) = do
   let res = parse T.bashCommandP x
   case res of
@@ -55,5 +59,69 @@ parseLines (x : xs) = do
   parseLines xs
 parseLines [] = return ()
 
--- >>> S.runState (parseLines ["x=3", "y=4", "ls -l -r"]) Map.empty
+-- >>> runStateT (parseLines ["a=1", "b=2"]) Map.empty
+-- ((),fromList [(V "a",Val (IntVal 1)),(V "b",Val (IntVal 2))])
+
+-- | displays the error message
+errorS :: Show a => a -> String
+errorS cmd = "Error: Unable to Parse Command " ++ show cmd
+
+evalLine :: (MonadError String m, MonadState (Map Var Expression) m) => String -> m BashCommand
+evalLine s = do
+  let res = parse T.bashCommandP s
+  case res of
+    Left err -> throwError $ errorS err
+    Right bc -> do
+      updHistory bc
+      return bc
+
+evalAllLines :: (MonadError String m, MonadState (Map Var Expression) m) => [String] -> m BashCommand
+evalAllLines [x] = do
+  evalLine x
+evalAllLines (x : xs) = do
+  evalLine x
+  evalAllLines xs
+evalAllLines [] = undefined
+
+showSt :: (a -> String) -> (a, Map Var Expression) -> String
+showSt f (v, map) = f v ++ ", map: " ++ show map
+
+-- | Show the result of runExceptT, parameterized by a function
+-- to show the value
+showEx :: (a -> String) -> Either ParseResult a -> String
+showEx _ (Left m) = "Raise: " ++ m
+showEx f (Right v) = "Result: " ++ f v
+
+goExSt :: String -> String
+goExSt e =
+  evalLine e -- :: StateT Int (ExceptT String Identity) Int
+    & flip runStateT Map.empty
+    & runExceptT
+    & runIdentity
+    & showEx (showSt show)
+
+goExStAll :: [String] -> String
+goExStAll (x : xs) =
+  evalAllLines (x : xs) -- :: StateT Int (ExceptT String Identity) Int
+    & flip runStateT Map.empty
+    & runExceptT
+    & runIdentity
+    & showEx (showSt show)
+goExStAll [] = ""
+
+-- >>> goExStAll ["x=3", "y=4"]
+-- "Result: Assign (V \"y\") (Val (IntVal 4)), map: fromList [(V \"x\",Val (IntVal 3)),(V \"y\",Val (IntVal 4))]"
+
+goStEx :: String -> String
+goStEx e =
+  evalLine e -- :: ExceptT String (StateT Int Identity) Int
+    & runExceptT
+    & flip runStateT Map.empty
+    & runIdentity
+    & showSt (showEx show)
+
+-- >>> goExSt "x=3"
+-- "Result: Assign (V \"x\") (Val (IntVal 3)), map: fromList [(V \"x\",Val (IntVal 3))]"
+
+-- >> runStateT (parseLines ["x=3", "y=4", "ls -l -r"]) Map.empty
 -- ((),fromList [(V "x",Val (IntVal 3)),(V "y",Val (IntVal 4))])
